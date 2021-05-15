34f7eccef1b1299a5da35ef05c28bb88b14194db
pri <prishapanchmia@gmail.com>
2021-05-15 16:19:18 +0530

    final changes

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import os
 import random
 from gtts import gTTS
 import webbrowser
-from datetime import datetime
+import datetime
 
 class user:
     name = ''
     def setName(self, name):
         self.name = name
 
-def there_exists(terms):
+def if_contains(terms):
     for term in terms:
         if term in voice_data:
             return True
@@ -33,12 +33,12 @@ def record_audio(ask=False):
         except sr.RequestError:
             lucy_speak('Sorry, the service is down') # error: recognizer is not connected
         print(f"You: {voice_data.lower()}") # print what user said
-        return voice_data.lower()
+        return voice_data.lower()  
 
 # get string and make a audio file to be played
 def lucy_speak(audio_string):
     tts = gTTS(text=audio_string, lang='en') # text to speech(voice)
-    r = random.randint(1,10000000)
+    r = random.randint(1,20000000)
     audio_file = 'audio' + str(r) + '.mp3'
     tts.save(audio_file) # save as mp3
     playsound.playsound(audio_file) # play the audio file
@@ -48,69 +48,63 @@ def lucy_speak(audio_string):
 
 def respond(voice_data):
     # 1: greetings
-    if there_exists(['hey','hi','hello']):
+    if if_contains(['hey','hi','hello']):
         greetings = [f"Hey, how can I help you?", f"Hey, what's up?", f"I'm listening.", f"How can I help you?"]
         greet = greetings[random.randint(0,len(greetings)-1)]
         lucy_speak(greet)
 
     # 2: name
-    if there_exists(["what is your name","what's your name","tell me your name"]):
+    if if_contains(["what is your name","what's your name","tell me your name"]):
         if user_obj.name:
             lucy_speak("My name is Lucy.")
         else:
             lucy_speak("My name is Lucy. What's your name?")
     # 3: user name
-    if there_exists(["my name is"]):
+    if if_contains(["my name is"]):
         user_name = voice_data.split("is")[-1].strip()
         lucy_speak(f"Okay, i will remember that {user_name}.")
         user_obj.setName(user_name) # remember name in user object
 
     # 4: greeting
-    if there_exists(["how are you","how are you doing"]):
+    if if_contains(["how are you","how are you doing"]):
         lucy_speak(f"I'm very well, thanks for asking {user_obj.name}.")
 
     # 5: time
-    if there_exists(["what's the time","tell me the time","what time is it","i need the time"]):
-        time = ctime().split(" ")[3].split(":")[0:2]
-        if time[0] == "00":
-            hours = '12'
-        else:
-            hours = time[0]
-        minutes = time[1]
-        time = f'{hours} {minutes}'
-        lucy_speak(time)
+    if if_contains(["what's the time","tell me the time","what time is it","i need the time"]):
+        strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+        lucy_speak(f"The time is {strTime}")
     
     # 6: search
-    if there_exists(["find"]):
+    if if_contains(["find"]):
         search = record_audio('What do you want to search for?')
         url = 'https://google.com/search?q=' + search
         webbrowser.get().open(url)
         lucy_speak('Here is what I found for ' + search)
 
     # 7: location
-    if there_exists(["find location"]):
+    if if_contains(["location"]):
         location = record_audio('What is the location?')
         url = 'https://google.nl/maps/place/' + location + '/&amp;'
         webbrowser.get().open(url)
         lucy_speak('Here is the location of ' + location)
     
     # 8: open notepad
-    if there_exists(["open notepad"]):
+    if if_contains(["open notepad"]):
         os.system('notepad')
         lucy_speak('Done for you!')
 
     # 9: youtube
-    if there_exists(["open youtube"]):
+    if if_contains(["open youtube"]):
             webbrowser.open("www.youtube.com")
             lucy_speak("Here is youtube for you!")
 
     # 10: github
-    if there_exists(["open github"]):
+    if if_contains(["open github"]):
             webbrowser.open("https://www.github.com")
             lucy_speak("Here is github for you!")
     
     # 11: exiting
-    if there_exists(["thank you","exit"]):
+    if if_contains(["thank you","exit"]):
         lucy_speak('Goodbye')
         exit()
     
